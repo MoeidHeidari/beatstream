56b8bcb9d0b7f8b3af923270a96df1f62e02289d
Moeid Heidari <moeidheidari@mac.home>
2024-11-16 19:50:57 +0100

    deploy configs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/beatstream.app/' : '', // Prefix for static assets
+  basePath: process.env.NODE_ENV === 'production' ? '/beatstream.app' : '', // Base path for GitHub Pages
   images: {
     remotePatterns: [
       {
@@ -8,6 +10,7 @@ const nextConfig = {
         hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
       },
     ],
+    unoptimized: true, // Ensures images work with static export
   },
 };
 
